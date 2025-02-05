fdf25b13879b80ecbf55f9a6009f4ff65a6cfd12
ammar <ammahmed004@gmail.com>
2025-02-05 19:26:59 +0100

    finished

--- algorithms/minimax.py
+++ algorithms/minimax.py
@@ -1,36 +1,47 @@
 from .trie import Trie
 
-
 class Minimax:
     def __init__(self, n, m):
-        self.trie = Trie()
-        self.grid = []
+        self.trie = Trie(n,m)
+        self.trie1 = Trie(1,1)
+        self.words=[]
+        self.grid = [['.'] * m for _ in range(n)]
         self.n = n
         self.m = m
 
-    def minimax(self, i: int, j: int, s: str) -> (int, str):
+    def insert(self, s, sc):
+        self.words.append((s, sc))
+
+    def grid_gen(self):
+        for word, score in self.words:
+            self.trie.insert(word, score)
+            self.trie1.insert(word[::-1], score)
+        self.grid = self.trie.grid_gen()
+        return self.grid
+
+    def minimax(self ,i: int, j: int, s: str) -> (int, str):
         score = 0
         s = self.grid[i][j] + s
-        score += self.trie.work(s)
+        score += self.trie1.work(s)
         if i == self.n - 1 and j == self.m - 1:
             return (score, 'F')
         aux1 = -1
         aux2 = -1
         if i < self.n - 1:
-            aux1, _ = self.minimax(i + 1, j, s)
+            aux1, move = self.minimax(i + 1, j, s)
         if j < self.m - 1:
-            aux2, _ = self.minimax(i, j + 1, s)
+            aux2, move = self.minimax(i, j + 1, s)
         if aux1 == -1:
-            return aux2 + score, 'D'
+            return aux2 + score, 'R'
         elif aux2 == -1:
-            return aux1 + score, 'R'
+            return aux1 + score, 'D'
         if (i + j) % 2:
             if aux1 > aux2:
-                return aux1 + score, 'R'
+                return aux1 + score, 'D'
             else:
-                return aux2 + score, 'D'
+                return aux2 + score, 'R'
         else:
             if aux1 < aux2:
-                return aux1 + score, 'R'
+                return aux1 + score, 'D'
             else:
-                return aux2 + score, 'D'
+                return aux2 + score, 'R'
